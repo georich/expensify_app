65b69eb308d3d8b0f925ac8c1439a1d5a203277b
George Richards <g.richards101@gmail.com>
2018-04-20 20:39:07 +0100

    understanding of reducers and combine reducers

--- example_code/redux_101.js
+++ example_code/redux_101.js
@@ -21,7 +21,11 @@ const resetCount = () => ({
   type: 'RESET'
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
       // const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
@@ -44,7 +48,10 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state;
   }
-});
+};
+
+// Creates redux store
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());  
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './example_code/redux_101.js',
+  entry: './example_code/redux_expensify.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
