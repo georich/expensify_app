25a08753e5f45fe77c163abb79256c1313a1bcd9
George Richards <g.richards101@gmail.com>
2018-04-21 13:03:04 +0100

    refactoring redux code and adding it to main project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+console.log(store.getState());
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 200, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 100, createdAt: 2000 }));
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+console.log(getVisibleExpenses(state.expenses, state.filters));
+
+store.dispatch(setTextFilter('water'));
+
+const stateTwo = store.getState();
+console.log(getVisibleExpenses(stateTwo.expenses, stateTwo.filters));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
